0da86a8b71bdbc173870c9a6dfcf37f77db54457
heartlinbenit <heartlinbenit@gmail.com>
2024-11-16 12:59:53 +0530

    DIO2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from 'react'
+import Speciality from './Speciality'
 
 function App() {
-
+  return (
+    <div>
+        <Speciality />
+    </div>
+  )
 }
+
 export default App
