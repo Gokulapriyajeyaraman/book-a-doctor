3662524c7fd775d7a246c06d41267d44ab0d6362
heartlinbenit <heartlinbenit@gmail.com>
2024-11-16 22:15:07 +0530

    DIO12

--- src/Doctors.jsx
+++ src/Doctors.jsx
@@ -17,12 +17,12 @@ function Doctors({id, doctorName, place, yearsOfExperience, profession}) {
                             <p className="mb-3 text-2xl font-normal text-gray-700 dark:text-gray-400">{doctorName}</p>
                             <p classNameName='text-sm'>{profession}</p>
                             <div className='m-2'>
-                                <p className='text-sm'>{place}</p>
+                                <p className='text-sm'>Location : {place}</p>
                                 <p className='text-sm'>Experience : {yearsOfExperience}</p>
                             </div>
                         </div>
 
-                        <a href="#" className="inline-flex justify-around items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-10 w-40">
+                        <a href="#" className="inline-flex justify-around items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 h-10 w-40">
                             Book Doctor
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
