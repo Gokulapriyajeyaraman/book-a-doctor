d8fd22237fde375f7791ebc98c5170dd6371919a
Gokulapriyajeyaraman <gokulapriyajeyaraman@gmail.com>
2024-11-17 03:08:29 +0530

    msg6

--- src/Booking.jsx
+++ src/Booking.jsx
@@ -18,17 +18,17 @@ function Booking() {
       <div className="flex flex-wrap flex-col pb-5 place-self-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
       
           <div class="flex flex-col justify-between p-4 leading-normal">
-          <img  src='./public/images/doc.jpg' />
+          <div  className = 'w-100 h-40 bg-cover'style={{ backgroundImage: `url(./public/images/doc.jpg)` }}></div>
           <h1 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Book Appointment</h1>
-          <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'> For hospital & clinical admissions.</p>
+          <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'> For  the hospital & clinical admissions.</p>
           <button className='text-white bg-teal-700 hover:bg-teal-900   font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ' onClick={handleclick}>FIND DOCTORS NOW</button>
           </div>
         </div>
 
         <div className="flex flex-wrap flex-col pb-5 place-self-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
           
-          <div class="flex flex-col justify-between p-4 leading-normal">
-          <img src='./public/images/med.jpg' width={275}  />
+          <div class="flex flex-col flex-wrap justify-between p-4 leading-normal">
+          <div  className = 'w-100 h-40 bg-cover'style={{ backgroundImage: `url(./public/images/med.jpg)` }}></div>
           <h1 className='mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Buy Medicine</h1>
           <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>We got all your health needs covered!</p>
           <button className='text-white bg-teal-700 hover:bg-teal-900  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>BUY NOW</button>
