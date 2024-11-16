015daeb5102ff3ee33236c3328e142c44efa4d9c
CarmelRiniha <rinirinal106@gmail.com>
2024-11-16 19:26:27 +0530

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-white border-blue-200 dark:bg-blue-900 fixed w-full h-20 z-10 top-0 left-0">
+      <nav className="bg-white border-teal-200 dark:bg-teal-900 fixed w-full h-20 z-10 top-0 left-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex justify-center items-center">
             <h1 className="flex items-center text-3xl dark:text-white">
@@ -16,7 +16,7 @@ const Navbar = () => {
 
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white-500 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-white-400 dark:hover:bg-blue-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white-500 rounded-lg md:hidden hover:bg-teal-100 focus:outline-none focus:ring-2 focus:ring-teal-200 dark:text-white-400 dark:hover:bg-teal-700 dark:focus:ring-gray-600"
           >
             <div className="space-y-1">
               <div className="w-6 h-0.5 bg-white"></div>
@@ -31,11 +31,11 @@ const Navbar = () => {
             } w-full md:block md:w-auto`}
             id="navbar-default"
           >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  border-white-100  md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white z-30  md:dark:bg-blue-900">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  border-white-100  md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white z-30  md:dark:bg-teal-900">
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-white bg-blue-600 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  className="block py-2 px-3 text-white bg-teal-600 rounded md:bg-transparent md:text-teal-700 md:p-0 dark:text-white md:dark:text-teal-500"
                   aria-current="page"
                 >
                   Home
@@ -44,7 +44,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Doctors
                 </a>
@@ -52,7 +52,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Book Test
                 </a>
@@ -60,7 +60,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Order Medicine
                 </a>
