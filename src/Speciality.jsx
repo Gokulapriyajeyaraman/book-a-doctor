f74166f9ac7e9d0b2c0958dfb7359ea1c860d4cb
heartlinbenit <heartlinbenit@gmail.com>
2024-11-16 13:53:23 +0530

    DIO#

--- src/Speciality.jsx
+++ src/Speciality.jsx
@@ -4,6 +4,26 @@ function Speciality() {
   return (
     <div>
         <p>Clinic and Specialities</p>
+        <div>
+            <div className='h-auto w-full'> 
+               <img class="rounded-full w-96 h-96" src="./urology.png" />
+            </div>
+            <div> 
+               <img class="rounded-full w-96 h-96" src="./neurology.png" />
+            </div>
+            <div> 
+               <img class="rounded-full w-96 h-96" src="./orthopedic.jpg" />
+            </div>
+            <div> 
+               <img class="rounded-full w-96 h-96" src="./cardiology.png" />
+            </div>
+            <div> 
+               <img class="rounded-full w-96 h-96" src="./consultant.png" />
+            </div>
+            <div> 
+               <img class="rounded-full w-96 h-96" src="./general.png" />
+            </div>      
+        </div>
     </div>
   )
 }
