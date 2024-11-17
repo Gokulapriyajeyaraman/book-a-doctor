ed9b3585f487f8319f7b35b31fd29717984a4626
CarmelRiniha <rinirinal106@gmail.com>
2024-11-17 19:39:32 +0530

    lightlogo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,18 +5,23 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-white border-teal-200 bg-teal-900 fixed w-full h-20 z-10 top-0 left-0">
+      
+      <nav
+  className="fixed w-full h-22 z-10 top-0 left-0   bg-teal-900"
+ 
+>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pb-2 p-4">
           <div className="flex justify-center items-center">
-          <img src="./public/images/doclogo1.png" class="h-16" alt="doctorly Logo" />
-            <h1 className="flex items-center text-3xl pl-2 text-white">
+          <img src="./public/images/lightlogo.png" class="h-16" alt="doctorly Logo" />
+            <h1 className="flex items-center text-3xl pl-4 pb-2 font-semibold text-white">
                BOOK A DOCTOR
             </h1>
           </div>
 
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white-500 rounded-lg md:bg-white bg-teal-700 hover:bg-teal-100 focus:outline-none focus:ring-2 focus:ring-teal-200 text-white-400 hover:bg-teal-700 focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center rounded-lg bg-teal-700 hover:bg-teal-600 text-white focus:outline-none focus:ring-2 focus:ring-teal-200"
+
           >
             <div className="space-y-1">
               <div className="w-6 h-0.5 bg-white"></div>
@@ -31,7 +36,7 @@ const Navbar = () => {
             } w-full md:block md:w-auto`}
             id="navbar-default"
           >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border-white-100 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white z-30 md:bg-teal-900">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  pb-2 border-white-100 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white z-30 md:bg-teal-900">
               <li>
                 <a
                   href="#"
